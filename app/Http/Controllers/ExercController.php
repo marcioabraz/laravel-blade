c91860d65eeaaa1b820938decd59e443e71e3c85
= <=>
2022-01-27 18:33:53 -0300

    teste

--- app/Http/Controllers/ExercController.php
+++ app/Http/Controllers/ExercController.php
@@ -18,14 +18,16 @@ class ExercController extends Controller
 
     public function images(Request $request)
     {
-        $request->image1->storeAs('public.images', 'file1.jpg');
-        $request->image2->storeAs('public.images', 'file2.jpg');
+        $request->image1->storeAs('public/images', 'file1.jpg');
+        $request->image2->storeAs('public/images', 'file2.jpg');
         return 'ok';
     }
     public function document(Request $request)
 
     {
         $request->file->store('public.documents');
+
+        $path = $request->file->storeAs('public/documents',$request->file->name);
  
         return 'deu certo';
 
